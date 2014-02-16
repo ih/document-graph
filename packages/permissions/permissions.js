128d03658af681df71120f85083b9464183a8b24
ih <irvin.hwang@gmail.com>
2014-02-15 16:47:46 -0800

    start permissions package

---
+++ packages/permissions/permissions.js
@@ -0,0 +1,8 @@
+PermissionsAPI = {
+	allActions = ['read', 'write'],
+	hasPermission: function (actorId, targetId, actions) {
+		// allows actions to be a single action for convenience
+		[].concat(actions);
+		
+	}
+};
