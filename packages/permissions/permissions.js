9e130faa120bbc0a175e09db3e8d2883ba230903
ih <irvin.hwang@gmail.com>
2014-03-19 19:57:49 -0700

    remove unused permissions package

--- packages/permissions/permissions.js
+++
@@ -1,7 +0,0 @@
-PermissionsAPI = {
-	allActions: ['read', 'write'],
-	hasPermission: function (actorId, targetId, actions) {
-		// allows actions to be a single action for convenience
-		[].concat(actions);
-	}
-};
