387ddaeab76af8ab8efff33a8a60249b4c1988d0
Irvin Hwang <irvin.hwang@gmail.com>
2014-09-24 20:54:13 -0700

    add test for cell selection

--- tests/suites/test05-mondrian.js
+++ tests/suites/test05-mondrian.js
@@ -23,7 +23,6 @@ casper.test.begin('Cell division', function suite(test) {
 	// check that horizontal division works
 	casper.then(function () {
 		var leafCellIds = this.getElementsAttribute('.leaf-cell', 'id');
-		this.echo('The leaves ' + leafCellIds);
 		this.click('#'+leafCellIds[0]+' .divide-horizontal');
 	});
 	casper.then(function () {
@@ -31,6 +30,48 @@ casper.test.begin('Cell division', function suite(test) {
 		test.assertElementCount('.leaf-cell', 3);
 		test.assertElementCount('.column-cell', 2);
 		test.assertElementCount('.row-cell', 2);
+	});
+
+	casper.run(function () {
+		test.done();
+	});
+});
+
+casper.test.begin('Cell Selection', function suite(test) {
+	var leafCellIds;
+	casper.start(SERVER, function () {
+		this.viewport(1200, 768);
+	});
+	logout(test);
+	login(test, dummyUsers.A);
+	// check the only cell is focused on
+	casper.then(function () {
+		test.assertElementCount('.focused', 1);
+	});
+	casper.then(function () {
+		this.click('.divide-vertical');
+	});
+	casper.then(function () {
+		this.click('.divide-horizontal');
+	});
+	// check that clicking each cell puts the focus on that cell
+	casper.then(function () {
+		leafCellIds = this.getElementsAttribute('.leaf-cell', 'id');
+		this.click('#'+leafCellIds[0]);
+	});
+	casper.then(function () {
+		test.assertElementCount('.focused', 1);
+		test.assertEquals(
+			this.getElementAttribute('.focused', 'id'), leafCellIds[0]);
+	});
+
+	casper.then(function () {
+		this.click('#'+leafCellIds[1]);
+	});
+	casper.then(function () {
+		test.assertElementCount('.focused', 1);
+		test.assertEquals(
+			this.getElementAttribute('.focused', 'id'), leafCellIds[1]);
 	});
 
 	casper.run(function () {
