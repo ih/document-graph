53cf09de1d64b4ad73a70f6df3b39350be425b96
Irvin Hwang <irvin.hwang@gmail.com>
2014-10-02 18:55:06 -0700

    get rid of state variable in editor

--- packages/editor/editor.js
+++ packages/editor/editor.js
@@ -1,53 +1,31 @@
 /**
  * Editor gets exported and acts as the interface to the editor component
  */
-Template.editor.created = function () {
-	this.state = new ReactiveDict();
-	this.state.set('mode', 'create');
-};
 
+// maybe this isn't needed...
 var Editor = {
 	initialize: function (mode, params) {
 		console.log('initializing the editor');
-		// templateInstance.state.set('mode', mode);
-		// TagEditor.initialize();
-	},
-	/**
-	 * Allows the editor to be closed externally and clears the state
-	 * e.g. clicking outside the editor
-	 */
-	close: function () {
 	}
 };
 
-Template.editor.helpers({
-	nodeContent: function () {
-		return Template.instance().state.get('nodeContent');
-	},
-	title: function () {
-		return Template.instance().state.get('title');
-	}
-});
-
 Template.editor.events({
 	'input .content': function (event, templateInstance) {
-		templateInstance.state.set('nodeContent', event.target.value);
+		templateInstance.nodeContent = event.target.value;
 	},
 	'input .title': function (event, templateInstance) {
-		templateInstance.state.set('title', event.target.value);
+		templateInstance.title = event.target.value;
 	},
 	'click .save': function (event, templateInstance) {
-		if (templateInstance.state.get('mode') === 'create') {
-			var privacySettings = PrivacyEditor.getPrivacySettings();
-			// the keys property of a reactive dict is basically the plain dict
-			var nodeId = GraphAPI.createNode(
-				// TODO this.keys probably isn't right
-				_.pick(templateInstance.state.keys, GraphAPI.nodeProperties), privacySettings);
-			_.each(TagEditor.getTags(), function (tag) {
-				TagsAPI.createTag({'objectId': nodeId, 'tag': tag});
-			});
-			resetEditor(templateInstance.state);
-		}
+		var privacySettings = PrivacyEditor.getPrivacySettings();
+		// the keys property of a reactive dict is basically the plain dict
+		var nodeData = _.pick(
+			templateInstance, GraphAPI.nodeProperties);
+		var nodeId = GraphAPI.createNode(nodeData, privacySettings);
+		_.each(TagEditor.getTags(), function (tag) {
+			TagsAPI.createTag({'objectId': nodeId, 'tag': tag});
+		});
+		resetEditor(templateInstance);
 	}
 });
 
@@ -55,16 +33,15 @@ Template.editor.rendered = function () {
 	$('textarea').autogrow();
 };
 
-function resetEditor(state) {
+function resetEditor(templateInstance) {
 	// used for resetting the title input, would be nice if there was two-way
 	// binding...
 	$('input').val('');
 
-	_.each(GraphAPI.nodeProperties, function (stateProperty) {
+	_.each(GraphAPI.nodeProperties, function (nodeProperty) {
 		// TODO figure out a better way to set the default value
-		state.set(stateProperty, '');
+		templateInstance[nodeProperty] = '';
 	});
 	TagEditor.clearTags();
 	console.log('cleared state');
-	console.log(state.keys);
 }
